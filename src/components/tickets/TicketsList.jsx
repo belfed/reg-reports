7c0de6d561c3a215aeedbb5c89c3cdea5c2b0657
belfed <federico.belotti@outlook.com>
2024-03-05 15:05:20 +0100

    Fixed overall layout: - Handled components centering on page level - Reorganized root component layout

--- src/components/tickets/TicketsList.jsx
+++ src/components/tickets/TicketsList.jsx
@@ -4,84 +4,89 @@ import { useLoaderData } from "react-router-dom";
 import { useCompositeStore } from "../../stores/store";
 
 import Ticket from "./Ticket";
-import { useEffect } from "react";
 
 // Temporary mock data.
 const mockData = [
-    {
-        id: "1",
-        title: "Task 1",
-        customer: "Customer A"
-    },
-    {
-        id: "2",
-        title: "Task 2",
-        customer: "Customer B"
-    },
-    {
-        id: "3",
-        title: "Task 3",
-        customer: "Customer C"
-    },
-    {
-        id: "4",
-        title: "Task 4",
-        customer: "Customer D"
-    },
-    {
-        id: "5",
-        title: "Task 5",
-        customer: "Customer E"
-    },
-    {
-        id: "6",
-        title: "Task 6",
-        customer: "Customer F"
-    },
-    {
-        id: "7",
-        title: "Task 7",
-        customer: "Customer G"
-    },
-    {
-        id: "8",
-        title: "Task 8",
-        customer: "Customer H"
-    },
-    {
-        id: "9",
-        title: "Task 9",
-        customer: "Customer I"
-    },
-    {
-        id: "10",
-        title: "Task 10",
-        customer: "Customer J"
-    }
-]
+  {
+    id: "1",
+    title: "Task 1",
+    customer: "Customer A",
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    customer: "Customer B",
+  },
+  {
+    id: "3",
+    title: "Task 3",
+    customer: "Customer C",
+  },
+  {
+    id: "4",
+    title: "Task 4",
+    customer: "Customer D",
+  },
+  {
+    id: "5",
+    title: "Task 5",
+    customer: "Customer E",
+  },
+  {
+    id: "6",
+    title: "Task 6",
+    customer: "Customer F",
+  },
+  {
+    id: "7",
+    title: "Task 7",
+    customer: "Customer G",
+  },
+  {
+    id: "8",
+    title: "Task 8",
+    customer: "Customer H",
+  },
+  {
+    id: "9",
+    title: "Task 9",
+    customer: "Customer I",
+  },
+  {
+    id: "10",
+    title: "Task 10",
+    customer: "Customer J",
+  },
+];
 
 const TicketsList = () => {
-    const tickets = useCompositeStore((state) => state.tickets);
-    const setTickets = useCompositeStore((state) => state.setTickets);
+  const tickets = useCompositeStore((state) => state.tickets);
+  const setTickets = useCompositeStore((state) => state.setTickets);
 
-    setTickets(tickets.length === 0 ? useLoaderData() || mockData : tickets);
+  const loadedTickets = useLoaderData();
 
-    return (
-        <Container>
-            {
-                tickets?.map((ticket) => <Ticket key={ticket.id} ticket={ticket} />)
-            }
-        </Container>
+  if (tickets.length === 0) {
+    setTickets(
+      !loadedTickets || loadedTickets.length === 0 ? mockData : loadedTickets
     );
+  }
+
+  return (
+    <Container>
+      {tickets?.map((ticket) => (
+        <Ticket key={ticket.id} ticket={ticket} />
+      ))}
+    </Container>
+  );
 };
 
 const Container = styled.div`
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    row-gap: 5px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  row-gap: 5px;
 
-    width: 100%;
+  width: 100%;
 `;
 
 export default TicketsList;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,34 @@
 import { Navigate } from "react-router-dom";
+import styled from "styled-components";
 
 import LoginForm from "../components/login/LoginForm";
 
 import { getItemFromLocalStorage } from "../data/login-api";
 
 const LoginPage = () => {
-    const isLoggedIn = getItemFromLocalStorage("username");
+  const isLoggedIn = getItemFromLocalStorage("username");
 
-    return isLoggedIn ? <Navigate replace to="/tickets" /> : <LoginForm />
-}
+  if (isLoggedIn) {
+    return <Navigate replace to="/tickets" />;
+  }
+
+  return (
+    <Container>
+      <LoginForm />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  box-sizing: border-box;
+
+  min-width: 100lvw;
+  min-height: 100lvh;
+`;
 
 export default LoginPage;
