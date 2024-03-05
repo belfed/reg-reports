7215e4f902667990e5c70f21bc6b009e1318ad28
belfed <federico.belotti@outlook.com>
2024-03-05 15:19:01 +0100

    Refactored tickets response: - Changed field names - User is now retrieved during the URL construction

--- src/data/tickets-api.js
+++ src/data/tickets-api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getItemFromLocalStorage } from "./login-api";
 
 /**
  * Asynchronously retrieves a single ticket based on the provided ID.
@@ -7,9 +8,11 @@ import axios from "axios";
  * @returns {Promise<object>} A promise that resolves to the ticket data.
  */
 export const getTicket = async (id) => {
-    const response = await axios.get(`${import.meta.env.VITE_TICKETS_URL}&$filter=(IDTodoList eq ${id})`).data.d;
+  const response = await axios.get(
+    `${import.meta.env.VITE_TICKETS_URL}&$filter=(IDTodoList eq ${id})`
+  ).data.d;
 
-    return response.data.d;
+  return response.data.d;
 };
 
 /**
@@ -18,8 +21,21 @@ export const getTicket = async (id) => {
  * @param {string} username - The username used for filtering tickets.
  * @returns {Promise<object>} A promise that resolves to the ticket data.
  */
-export const getTickets = async (username) => {
-    const response = await axios.get(`${import.meta.env.VITE_TICKETS_URL}&$filter=(substringof('${username}', AssegnatoA) and Status ne 90 and substringof('√', AssegnatoA) eq false)`);
+export const getTickets = async () => {
+  const response = await axios.get(
+    `${
+      import.meta.env.VITE_TICKETS_URL
+    }&$filter=(substringof('${getItemFromLocalStorage(
+      "username"
+    )}', AssegnatoA) and Status ne 90 and substringof('√', AssegnatoA) eq false)`
+  );
 
-    return response.data.d.map((ticket) => ({ ...ticket, isRunning: false }));
+  return response.data.d.map((ticket) => ({
+    id: ticket.IDTodoList,
+    customer: ticket.Cliente,
+    title: ticket.Titolo,
+    description: "",
+    elapsedTime: 0,
+    isRunning: false,
+  }));
 };
